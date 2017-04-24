d193dd34d9a1ace39337c14c22e60f34bab40569
qbingham <quinbingham@gmail.com>
2017-04-23 20:17:22 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,45 @@
-
 public class Player {
-
+	Hand playerHand;
+	int pHealth = 100;
+	int dNumber = 5;
+	int dSides = 6;
+	int dTurns = 3;
+	
+	
+	int getpHealth(){
+		return pHealth;
+	}
+	
+	int getdNumber(){
+		return dNumber;
+	}
+	
+	int getdSides(){
+		return dSides;
+	}
+	
+	int getdTurns(){
+		return dTurns;
+	
+	}
+	
+	void setHealth(int pHealth){
+		this.pHealth = pHealth;
+	}
+	
+	void setdNumber(int dNumber){
+		this.dNumber = dNumber;
+	}
+	
+	void setdSides(int dSides){
+		this.dSides = dSides;
+	}
+	
+	void setdTurns(int dTurns){
+		this.dTurns = dTurns;
+	}
+	
+	void updateDamage(int damage){
+		this.pHealth = (this.pHealth - damage);
+	}
 }
