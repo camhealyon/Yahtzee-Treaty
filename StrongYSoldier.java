c7d9db292df8b03c0b05aaff8640bf9bb5c0aabf
qbingham <quinbingham@gmail.com>
2017-04-23 21:26:57 -0700

    Update StrongYSoldier.java

--- StrongYSoldier.java
+++ StrongYSoldier.java
@@ -9,7 +9,7 @@ public class StrongYSoldier extends YahtzeePEnemy{
 		setnT(1);
 		set = new Settings(getnS(), getnD(), getnT()); 
 		EnemyHand= new Hand(set);
-		for (int i = 0; i < getnS(); i++){
+		for (int i = 0; i < getnD(); i++){
 			EnemyHand.addDie();
 		}
 	}
